16a55003bb27c493016e90adb56c71c6c3f089bb
Animator55 <animator55developer@gmail.com>
2024-11-22 18:24:56 -0300

    Update Notifications.tsx

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -10,60 +10,60 @@ type Props = {
 }
 
 export default function Notifications({ close, EditMassiveTable }: Props) {
-  const [list, setList]= React.useState<SingleEvent[] | undefined>(undefined)
+  const [list, setList] = React.useState<SingleEvent[] | undefined>(undefined)
 
   const [pop, OpenPop] = React.useState<SingleEvent | undefined>()
 
-  const requestNotifications = ()=>{
+  const requestNotifications = () => {
     let data = getNotificationsGeneral()
     setList(data)
   }
 
-  React.useEffect(()=>{
-    if(!list) setTimeout(()=>{requestNotifications()}, 300)
+  React.useEffect(() => {
+    if (!list) setTimeout(() => { requestNotifications() }, 300)
   }, [])
 
-  const Alert = ()=>{
+  const Alert = () => {
     return <section className='alert'>
       <FontAwesomeIcon icon={faWarning} />
       <h2>No hay notificaciones previas.</h2>
-  </section>
+    </section>
   }
 
-  const RenderList = ()=>{
-    if(!list || list.length === 0) return <Alert/>
+  const RenderList = () => {
+    if (!list || list.length === 0) return <Alert />
 
-    let ul= []
-    for(let i=0;i<list.length; i++) {
+    let ul = []
+    for (let i = 0; i < list.length; i++) {
       // if(i === 10) break
       let el = list[i]
-      const action = (boolean:boolean)=>{
-        setList([...list.map(item=>{
-          if(el === item) return {...el, accepted : boolean}
+      const action = (boolean: boolean) => {
+        setList([...list.map(item => {
+          if (el === item) return { ...el, accepted: boolean }
           else return item
         })])
-        if(boolean) EditMassiveTable(el._id, el.products, el.comment)
+        if (boolean) EditMassiveTable(el._id, el.products, el.comment)
       }
 
       const actionZone = {
         "undefined": <>
-          <button onClick={()=>{action(true)}}>
-            <FontAwesomeIcon icon={faCheck}/>
+          <button onClick={() => { action(true) }}>
+            <FontAwesomeIcon icon={faCheck} />
           </button>
-          <button onClick={()=>{action(false)}}>
-            <FontAwesomeIcon icon={faXmark}/>
+          <button onClick={() => { action(false) }}>
+            <FontAwesomeIcon icon={faXmark} />
           </button>
         </>,
-        "true": <FontAwesomeIcon icon={faCheck}/>,
-        "false": <FontAwesomeIcon icon={faXmark}/>,
+        "true": <FontAwesomeIcon icon={faCheck} />,
+        "false": <FontAwesomeIcon icon={faXmark} />,
       }
 
       ul.push(<li
         className='notification-item'
         key={Math.random()}
-        onClick={(e)=>{
+        onClick={(e) => {
           let target = e.target as HTMLDivElement
-          if(target.className === "notification-item")OpenPop(el)
+          if (target.className === "notification-item") OpenPop(el)
         }}
       >
         <p>Mesa {el.name}</p>
@@ -78,45 +78,54 @@ export default function Notifications({ close, EditMassiveTable }: Props) {
     </ul>
   }
 
-  const ViewNotification = ()=>{
-    if(!pop) return
+  const ViewNotification = () => {
+    if (!pop) return
     return <section className='back-blur' onClick={(e) => {
-        let target = e.target as HTMLDivElement
-        if (target.className === "back-blur") OpenPop(undefined)
+      let target = e.target as HTMLDivElement
+      if (target.className === "back-blur") OpenPop(undefined)
     }}>
       <section className='pop'>
         <header>
-            <div className='pop-top'>
-                <h2>Detalles</h2>
-                <button onClick={() => { OpenPop(undefined) }}><FontAwesomeIcon icon={faXmark} /></button>
-            </div>
+          <div className='pop-top'>
+            <h2>Detalles</h2>
+            <button onClick={() => { OpenPop(undefined) }}><FontAwesomeIcon icon={faXmark} /></button>
+          </div>
         </header>
         <section className='pop-content'>
           <div className='product-editor-content'>
-              <div>
-                  <h3>Mesa {pop.name}</h3>
-                  <p>{pop.timestamp}</p>
-                  {pop.owner_name && <p>Origen: <i>{pop.owner_name}</i></p>}
+            <div>
+              <h3>Mesa {pop.name}</h3>
+              <p>{pop.timestamp}</p>
+              {pop.owner_name && <p>Origen: <i>{pop.owner_name}</i></p>}
+            </div>
+            <hr></hr>
+
+            <div className='prod-container'>
+              <div className='top-result-prod'>
+                <div>Nombre</div>
+                <div>Precio</div>
+                <div>Cantidad</div>
               </div>
-              <hr></hr>
-              
-              <div className='prod-container'>
-                    <div className='top-result-prod'>
-                        <div>Nombre</div>
-                        <div>Precio</div>
-                        <div>Cantidad</div>
-                    </div>
-                    <ul className='result-prod-list'>
-                        {pop.products && pop.products.map(el => {
-                            return el.header ? <label key={Math.random()}>{el.type}</label> :
-                                <li key={Math.random()}>
-                                    <div>{el.name}</div>
-                                    <div>${el.price}</div>
-                                    <div>{el.amount}</div>
-                                </li>
-                        })}
+              <ul className='result-prod-list'>
+                {pop.products && pop.products.map((pha, i) => {
+                  return <section
+                    key={Math.random()}
+                  >
+                    <label>Fase {i + 1}</label>
+                    <ul>
+                      {pha.map(el => {
+                        return el.header ? <label key={Math.random()}>{el.type}</label> :
+                          <li key={Math.random()}>
+                            <div>{el.name}</div>
+                            <div>${el.price}</div>
+                            <div>{el.amount}</div>
+                          </li>
+                      })}
                     </ul>
-                </div>
+                  </section>
+                })}
+              </ul>
+            </div>
           </div>
         </section>
       </section>
@@ -127,11 +136,11 @@ export default function Notifications({ close, EditMassiveTable }: Props) {
     let target = e.target as HTMLDivElement
     if (target.className === "back") close()
   }}>
-    {pop && <ViewNotification/>}
+    {pop && <ViewNotification />}
     <section className='notifications'>
       {list === undefined ?
-        <div className='loading-notis'><FontAwesomeIcon icon={faCircleNotch} spin/></div>
-        : <RenderList/>
+        <div className='loading-notis'><FontAwesomeIcon icon={faCircleNotch} spin /></div>
+        : <RenderList />
       }
     </section>
   </section>
